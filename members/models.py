c97a245c7193de6ccbb77229b2455daf92ceb15c
Vaishnavi kadam <vaishanvi.k@sankeysolutions.com>
2024-06-28 10:44:05 +0530

    Initial commit

---
+++ members/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Member(models.Model):
+    firstName = models.CharField(max_length=255)
+    lastName = models.CharField(max_length=255)
+    phone = models.IntegerField(null=True)
+    joined_date = models.DateField(null=True)
---
+++ members/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    
+    path('', views.main, name='main'),
+    path('members/', views.members, name='members'),
+    path('members/details/<int:id>', views.details, name='details'),
+
+]
